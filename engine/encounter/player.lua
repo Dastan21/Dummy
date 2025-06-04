ba1fdb2e3ccd659f5633fc00991a2686b213ddf5
Dastan21 <loicd21@gmail.com>
2025-06-04 16:37:00 +0200

    rename player export

--- engine/encounter/player.lua
+++ engine/encounter/player.lua
@@ -9,52 +9,52 @@
 --- @field private speed_factor number
 --- @field private hitbox {[1]: number, [2]: number, [3]: number, [4]: number}
 --- @field private is_fleeing boolean
-local self = {}
+local Player = {}
 
 --- Inits the player
-function self.load()
-  self.lv = 1
-  self.hp = 20
-  self.max_hp = 20
-  self.at = 10
-  self.df = 10
-  self.speed = 4
-  self.speed_factor = 1
-  self.hitbox = { 4, 4, 8, 8 }
+function Player.load()
+  Player.lv = 1
+  Player.hp = 20
+  Player.max_hp = 20
+  Player.at = 10
+  Player.df = 10
+  Player.speed = 4
+  Player.speed_factor = 1
+  Player.hitbox = { 4, 4, 8, 8 }
 
-  self.soul_sprite = Sprite:new("heart")
-  self.soul_sprite:setPosition(320, 240)
-  self.soul_sprite:setLayer(Constants.LAYERS.SOUL)
+  Player.soul_sprite = Sprite:new("heart")
+  Player.soul_sprite:setPosition(320, 240)
+  Player.soul_sprite:setLayer(Constants.LAYERS.SOUL)
 
-  self.name = "FRISK"
-  self.name_text = Text:new(self.name)
-  self.name_text:setPosition(30, 400)
-  self.name_text:setOrigin(0)
-  self.name_text:setFont(Font.FONTS.CURS)
+  Player.name = "FRISK"
+  Player.name_text = Text:new(Player.name)
+  Player.name_text:setPosition(30, 400)
+  Player.name_text:setOrigin(0)
+  Player.name_text:setFont(Font.FONTS.CURS)
 
-  self.lv_text = Text:new("")
-  self.lv_text:setPosition(174, 400)
-  self.lv_text:setOrigin(0)
-  self.lv_text:setFont(Font.FONTS.CURS)
+  Player.lv_text = Text:new("")
+  Player.lv_text:setPosition(174, 400)
+  Player.lv_text:setOrigin(0)
+  Player.lv_text:setFont(Font.FONTS.CURS)
 
-  self.hp_sprite = Sprite:new("hp")
-  self.hp_sprite:setPosition(244, 405)
-  self.hp_sprite:setOrigin(0)
-  self.hp_value_text = Text:new("")
-  self.hp_value_text:setPosition(400, 409)
-  self.hp_value_text:setOrigin(0, 0.5)
-  self.hp_value_text:setFont(Font.FONTS.CURS)
+  Player.hp_sprite = Sprite:new("hp")
+  Player.hp_sprite:setPosition(244, 405)
+  Player.hp_sprite:setOrigin(0)
+  Player.hp_value_text = Text:new("")
+  Player.hp_value_text:setPosition(400, 409)
+  Player.hp_value_text:setOrigin(0, 0.5)
+  Player.hp_value_text:setFont(Font.FONTS.CURS)
 
-  self.is_fleeing = false
-  self.flee_speed = 3
+  Player.is_fleeing = false
+  Player.flee_speed = 3
 
-  self.setLV(1, true)
+  Player.setLV(1, true)
 
   Drawable:new(function()
-    if Debug.show_hitbox and not self.isHidden() then
-      local x, y = self.getPosition()
+    if Debug.show_hitbox and not Player.isHidden() then
+      local x, y = Player.getPosition()
       love.graphics.setColor(0, 1, 0, 1)
-      love.graphics.rectangle("line", x - self.hitbox[1], y - self.hitbox[2], self.hitbox[3], self.hitbox[4])
+      love.graphics.rectangle("line", x - Player.hitbox[1], y - Player.hitbox[2], Player.hitbox[3], Player.hitbox[4])
     end
   end):setLayer(Constants.LAYERS.ABOVE_SOUL)
 end
@@ -63,157 +63,157 @@ end
 --- @param x number horizontal position
 --- @param y number vertical position
 --- @param ignore_arena_bounds? boolean ignore arena bounds collisions
-function self.setPosition(x, y, ignore_arena_bounds)
+function Player.setPosition(x, y, ignore_arena_bounds)
   if not ignore_arena_bounds then
     local arena_x, arena_y = Arena.getPosition()
     local arena_width, arena_height = Arena.getWidth(), Arena.getHeight()
-    local player_offset = self.hitbox[3] - self.hitbox[1] + 4
+    local player_offset = Player.hitbox[3] - Player.hitbox[1] + 4
     x = math.clamp(x, arena_x - arena_width / 2 + player_offset, arena_x + arena_width / 2 - player_offset)
     y = math.clamp(y, arena_y - arena_height + player_offset, arena_y - player_offset)
   end
 
-  self.soul_sprite:setPosition(x, y)
+  Player.soul_sprite:setPosition(x, y)
 end
 
 --- Gets the player's soul position
 --- @return number x horizontal position
 --- @return number y vertical position
-function self.getPosition()
-  return self.soul_sprite:getPosition()
+function Player.getPosition()
+  return Player.soul_sprite:getPosition()
 end
 
 --- Shows the player's soul
-function self.show()
-  self.soul_sprite:setVisible(true)
+function Player.show()
+  Player.soul_sprite:setVisible(true)
 end
 
 --- Hides the player's soul
-function self.hide()
-  self.soul_sprite:setVisible(false)
+function Player.hide()
+  Player.soul_sprite:setVisible(false)
 end
 
 --- Wether the player's soul is hidden
 --- @return boolean
-function self.isHidden()
-  return not self.soul_sprite:isVisible()
+function Player.isHidden()
+  return not Player.soul_sprite:isVisible()
 end
 
 --- Sets the player's name
 --- @param name string name displayed
-function self.setName(name)
+function Player.setName(name)
   if name == nil then return end
 
-  self.name = Utils.getOrDefault(name, "Frisk")
-  self.name_text:setText(self.name)
-  self.lv_text:setPosition(self.name_text:getSprite():getWidth() + 57, 400)
+  Player.name = Utils.getOrDefault(name, "Frisk")
+  Player.name_text:setText(Player.name)
+  Player.lv_text:setPosition(Player.name_text:getSprite():getWidth() + 57, 400)
 end
 
 --- Gets the player's name
 --- @return string
-function self.getName()
-  return self.name
+function Player.getName()
+  return Player.name
 end
 
 --- Sets the player's LV
 --- @param lv number level
 --- @param heal? boolean set HP to max HP
-function self.setLV(lv, heal)
+function Player.setLV(lv, heal)
   if type(lv) ~= "number" then return end
 
-  self.lv = math.clamp(lv, 1, 20)
-  self.lv_text:setText(Lang.translate("ENCOUNTER_STAT_LV") .. " " .. tostring(self.lv))
+  Player.lv = math.clamp(lv, 1, 20)
+  Player.lv_text:setText(Lang.translate("ENCOUNTER_STAT_LV") .. " " .. tostring(Player.lv))
 
-  if self.lv < 20 then
-    self.setMaxHP(16 + 4 * self.lv)
+  if Player.lv < 20 then
+    Player.setMaxHP(16 + 4 * Player.lv)
   else
-    self.setMaxHP(99)
+    Player.setMaxHP(99)
   end
 
-  self.setHP(math.min(self.hp, self.max_hp))
+  Player.setHP(math.min(Player.hp, Player.max_hp))
   if heal == true then
-    self.setHP(self.max_hp)
+    Player.setHP(Player.max_hp)
   end
-  self.setAT(8 + 2 * self.lv)
-  self.setDF(9 + math.ceil(self.lv / 4))
+  Player.setAT(8 + 2 * Player.lv)
+  Player.setDF(9 + math.ceil(Player.lv / 4))
 end
 
 --- Gets the player's LV
 --- @return number
-function self.getLV()
-  return self.lv
+function Player.getLV()
+  return Player.lv
 end
 
 --- Sets the player's HP
 --- @param hp number health points
-function self.setHP(hp)
+function Player.setHP(hp)
   if type(hp) ~= "number" then return end
 
-  self.hp = math.clamp(hp, 0, math.min(self.max_hp, 99))
-  self.hp_value_text:setText(string.format("%02d", self.hp) .. " / " .. tostring(self.max_hp))
-  self.hp_value_text:setPosition(289 + math.clamp(5 * Player.getLV() + 20, 25, 120), 409)
+  Player.hp = math.clamp(hp, 0, math.min(Player.max_hp, 99))
+  Player.hp_value_text:setText(string.format("%02d", Player.hp) .. " / " .. tostring(Player.max_hp))
+  Player.hp_value_text:setPosition(289 + math.clamp(5 * Player.getLV() + 20, 25, 120), 409)
 end
 
 --- Gets the player's HP
 --- @return number
-function self.getHP()
-  return self.hp
+function Player.getHP()
+  return Player.hp
 end
 
 --- Sets the player's max HP
 --- @param max_hp number maximum health points
 --- @param heal? boolean set HP to max HP
-function self.setMaxHP(max_hp, heal)
+function Player.setMaxHP(max_hp, heal)
   if type(max_hp) ~= "number" then return end
 
-  self.max_hp = math.clamp(max_hp, 20, 99)
-  self.hp_value_text:setText(tostring(self.hp) .. " / " .. tostring(self.max_hp))
-  self.hp_value_text:setPosition(289 + math.clamp(5 * Player.getLV() + 20, 25, 120), 409)
+  Player.max_hp = math.clamp(max_hp, 20, 99)
+  Player.hp_value_text:setText(tostring(Player.hp) .. " / " .. tostring(Player.max_hp))
+  Player.hp_value_text:setPosition(289 + math.clamp(5 * Player.getLV() + 20, 25, 120), 409)
 
   if heal == true then
-    self.setHP(self.max_hp)
+    Player.setHP(Player.max_hp)
   end
 end
 
 --- Gets the player's AT
 --- @return number
-function self.getAT()
-  return self.at
+function Player.getAT()
+  return Player.at
 end
 
 --- Sets the player's AT
 --- @param at number attack point
-function self.setAT(at)
-  self.at = at
+function Player.setAT(at)
+  Player.at = at
 end
 
 --- Gets the player's DE
 --- @return number
-function self.getDF()
-  return self.df
+function Player.getDF()
+  return Player.df
 end
 
 --- Sets the player's DE
 --- @param df number defense point
-function self.setDF(df)
-  self.df = df
+function Player.setDF(df)
+  Player.df = df
 end
 
 --- Gets the player's speed
 --- @return number
-function self.getSpeed()
-  return self.speed_factor
+function Player.getSpeed()
+  return Player.speed_factor
 end
 
 --- Sets the player's speeds
 --- @param speed number
-function self.setSpeed(speed)
-  self.speed_factor = speed
+function Player.setSpeed(speed)
+  Player.speed_factor = speed
 end
 
 --- Wether the player's hitbox collides bullet's hitbox
 --- @param bullet Dummy.Bullet
-function self.isColliding(bullet)
-  local player_x, player_y = self.soul_sprite:getPosition()
+function Player.isColliding(bullet)
+  local player_x, player_y = Player.soul_sprite:getPosition()
   local bullet_scale = bullet:getScale()
   local bullet_origin = bullet:getOrigin()
   local bullet_hitbox = bullet:getHitbox()
@@ -221,42 +221,42 @@ end
 
 --- Gets the player's max HP
 --- @return number
-function self.getMaxHP()
-  return self.max_hp
+function Player.getMaxHP()
+  return Player.max_hp
 end
 
 --- Wether the playing is playing the escape animation
 ---@return boolean
-function self.isFleeing()
-  return self.is_fleeing
+function Player.isFleeing()
+  return Player.is_fleeing
 end
 
 --- Animates the soul escaping
-function self.flee(dt)
-  if not self.is_fleeing then
-    self.soul_escape_sprite = Sprite:new({ "heart_escape1", "heart_escape2" }, 2 / 30, true)
-    self.soul_escape_sprite:setPosition(self.getPosition())
-    self.soul_escape_sprite:setLayer(Constants.LAYERS.SOUL)
-    self.soul_sprite:setVisible(false)
-    self.is_fleeing = true
+function Player.flee(dt)
+  if not Player.is_fleeing then
+    Player.soul_escape_sprite = Sprite:new({ "heart_escape1", "heart_escape2" }, 2 / 30, true)
+    Player.soul_escape_sprite:setPosition(Player.getPosition())
+    Player.soul_escape_sprite:setLayer(Constants.LAYERS.SOUL)
+    Player.soul_sprite:setVisible(false)
+    Player.is_fleeing = true
   end
 
-  local x, y = self.soul_escape_sprite:getPosition()
-  self.soul_escape_sprite:setPosition(x - self.flee_speed * dt * 30, y)
+  local x, y = Player.soul_escape_sprite:getPosition()
+  Player.soul_escape_sprite:setPosition(x - Player.flee_speed * dt * 30, y)
 end
 
 --- Updates the player
 --- @param dt number
-function self.update(dt)
+function Player.update(dt)
   local dir_x, dir_y = 0, 0
   if Input.isDown(Input.Up) then dir_y = dir_y - 1 end
   if Input.isDown(Input.Down) then dir_y = dir_y + 1 end
   if Input.isDown(Input.Left) then dir_x = dir_x - 1 end
   if Input.isDown(Input.Right) then dir_x = dir_x + 1 end
 
-  local s = self.speed * dt * 30
-  local x, y = self.soul_sprite:getPosition()
-  self.setPosition(x + dir_x * s, y + dir_y * s)
+  local s = Player.speed * dt * 30
+  local x, y = Player.soul_sprite:getPosition()
+  Player.setPosition(x + dir_x * s, y + dir_y * s)
 end
 
-return self
+return Player
