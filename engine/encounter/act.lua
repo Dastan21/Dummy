b4b055dfcef13f1eadff42a613b06370363fb59b
Dastan21 <loicd21@gmail.com>
2025-06-13 23:48:39 +0200

    remove text from ACT

--- engine/encounter/act.lua
+++ engine/encounter/act.lua
@@ -1,7 +1,6 @@
 --- @class Dummy.ACT : Dummy.Class
 ---
 --- @field protected name Dummy.Text.Text
---- @field protected text Dummy.Text.Text|nil
 local ACT = Class()
 
 --- Gets the class name
@@ -16,24 +15,8 @@ function ACT:getName()
   return self.name
 end
 
---- Gets the ACT's dialogue text
---- @return Dummy.Text.Text
-function ACT:getText()
-  return self.text
-end
-
---- Sets the ACT's dialogue text
---- @param text Dummy.Text.Text
-function ACT:setText(text)
-  self.text = text
-end
-
 --- Called when the ACT is used
-function ACT:use()
-  if self.text ~= nil then
-    Encounter.playDialogue(self.text)
-  end
-end
+function ACT:use() end
 
 --- Creates an enemy ACTing
 --- @param name Dummy.Text.Text
